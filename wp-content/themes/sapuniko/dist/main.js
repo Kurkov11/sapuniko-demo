844f62b5a6ecb6864753f03657d6dd4d157bb12c
Damian Kurkowicz <kurkovsw@icloud.com>
2025-03-31 21:50:20 +0200

    commit

--- wp-content/themes/sapuniko/dist/main.js
+++ wp-content/themes/sapuniko/dist/main.js
@@ -600,13 +600,13 @@
                       },
                       defaults: { ease: "none" },
                     })
-                    .to(
+                    .fromTo(
                       A,
+                      { transform: "", boxShadow: "", duration: 0.3 },
                       {
                         transform: "scale(1.05)",
                         boxShadow:
                           "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-                        duration: 0.3,
                       },
                       "<"
                     )
